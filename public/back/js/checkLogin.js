f79727cb82f6839f6763e10c8bdc15501ba2dffc
shaowenzhi <274084964@qq.com>
2018-09-28 09:59:14 +0800

    首页完成

--- public/back/js/checkLogin.js
+++ public/back/js/checkLogin.js
@@ -1,13 +1,17 @@
 $.ajax({
   type: "get",
-  url: "/employee/employeeLogout",
+  url: "/employee/checkRootLogin",
   dataType: "json",
-  success: function(info) {
-    if(info.error === 400) {
+  success: function( info ) {
+    //console.log( info );
+    if ( info.error === 400 ) {
+      // 未登录, 拦截到登陆页
       location.href = "login.html";
     }
-    if(info.success) {
-      console.log("当前用户已经登录");
+
+    if ( info.success ) {
+      // 已登陆, 让用户继续访问即可
+      console.log( "当前用户已登陆" );
     }
   }
 })
