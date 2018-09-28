863dacb41a6f21806e5124720142620f4f5b676d
shaowenzhi <274084964@qq.com>
2018-09-28 15:17:05 +0800

    用户页面功能基本实现

---
+++ public/back/js/user.js
@@ -0,0 +1,32 @@
+$(function () {
+  var currentPage = 1;
+  var pageSize = 5;
+  render();
+  function render() {
+    $.ajax({
+      type: "get",
+      url: "/user/queryUser",
+      data: {
+        page: currentPage,
+        pageSize: pageSize
+      },
+      dataType: "json",
+      success: function (info) {
+        console.log(info);
+        var htmlStr = template("tmp", info);
+        $('tbody').html(htmlStr);
+  
+        $("#paginator").bootstrapPaginator({
+          bootstrapMajorVersion: 3, //默认是2，如果是bootstrap3版本，这个参数必填
+          currentPage: info.page, //当前页
+          totalPages: Math.ceil(info.total / info.size), //总页数
+          onPageClicked: function (event, originalEvent, type, page) {
+            //为按钮绑定点击事件 page:当前点击的按钮值
+            currentPage = page;
+            render();
+          }
+        });
+      }
+    });
+  }
+})
